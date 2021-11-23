60f5af281f6f64037086cc84344469c764a53acf
Chris Ford <nb19364@bristol.ac.uk>
2021-11-23 11:12:15 +0000

    updated client.py

--- client.py
+++ client.py
@@ -1,9 +1,15 @@
 import numpy as np
-import socket
 from mlsocket import MLSocket
-from main import numpysocket
+
 
 server_ip = '164.11.72.118'
-port = 9001
+port = 5777
 
-numpysocket.startClient(server_ip, port)
+array = np.random.randint(0,255, size=(150,150))
+
+with MLSocket() as s:
+    s.connect((server_ip, port))
+
+    while True:
+        array = np.random.randint(0,255, size=(150,150))
+        s.send(array)
